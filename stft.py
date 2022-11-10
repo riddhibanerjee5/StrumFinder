4ac88cca52aeed9923780967a8514538450095b2
Cyrus Vanden Bosch <58001252+cvbosch@users.noreply.github.com>
2022-11-10 01:44:54 -0500

    update stft with wav read and colormap

--- stft.py
+++ stft.py
@@ -1,5 +1,9 @@
-import numpy as np
+import soundfile as sf
 import math
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
+
 
 def stft(n, t, w, x, W, Fs):
     M = math.floor(len(x) / W)
@@ -13,25 +17,39 @@ def stft(n, t, w, x, W, Fs):
         w.append((2*math.pi*i)/float(W))
     return X
 
-#x =  [0.00, 0.59, 0.95, 0.95, 0.59, 0.00, -0.59, -0.95, -0.95, -0.59, 0.00, 0.95, 0.59, -0.59, -0.95, 0.00, 0.95, 0.59, -0.59, -0.95, 0.00, 0.95, -0.59, -0.59, 0.95, 0.00, -0.95, 0.59, 0.59, -0.95, 0.00, 0.59, -0.95, 0.95, -0.59, 0.00, 0.59, -0.95, 0.95, -0.59, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, -0.59, 0.95, -0.95, 0.59, 0.00, -0.59, 0.95, -0.95, 0.59, 0.00, -0.95, 0.59, 0.59, -0.95, 0.00, 0.95, -0.59, -0.59, 0.95, 0.00, -0.95, -0.59, 0.59, 0.95, 0.00, -0.95, -0.59, 0.59, 0.95, 0.00, -0.59, -0.95, -0.95, -0.59, 0.00, 0.59, 0.95, 0.95, 0.59, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00]
-W = 100
+def result():
+    for i in range(len(X)):
+        for j in range(len(X[i])):
+            if (abs(X[i][j]) < 0.0000001):
+                X[i][j] = 0
+            print("X[", i, "][", j, "]: ", X[i][j])
+
+W = 5000
 Fs = 44100.0
 n = []
 t = []
 w = []
 
-x = []
-file = open("chirp.txt")
-while True:
-    line = file.readline()
-    if not line:
-        break
-    x.append(float(line))
-file.close()
+#x =  [0.00, 0.59, 0.95, 0.95, 0.59, 0.00, -0.59, -0.95, -0.95, -0.59, 0.00, 0.95, 0.59, -0.59, -0.95, 0.00, 0.95, 0.59, -0.59, -0.95, 0.00, 0.95, -0.59, -0.59, 0.95, 0.00, -0.95, 0.59, 0.59, -0.95, 0.00, 0.59, -0.95, 0.95, -0.59, 0.00, 0.59, -0.95, 0.95, -0.59, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, -0.59, 0.95, -0.95, 0.59, 0.00, -0.59, 0.95, -0.95, 0.59, 0.00, -0.95, 0.59, 0.59, -0.95, 0.00, 0.95, -0.59, -0.59, 0.95, 0.00, -0.95, -0.59, 0.59, 0.95, 0.00, -0.95, -0.59, 0.59, 0.95, 0.00, -0.59, -0.95, -0.95, -0.59, 0.00, 0.59, 0.95, 0.95, 0.59, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00]
 
-X = stft(n,t,w,x,W,Fs)
+#file = open("chirp.txt")
+#while True:
+#    line = file.readline()
+#    if not line:
+#        break
+#    x.append(float(line))
+#file.close()
+
+x, Fs = sf.read('gods.wav')
+x = x.transpose()
+
+X = stft(n,t,w,x[1],W,Fs)
+
 for i in range(len(X)):
-    for j in range(len(X[i])):
-        if (abs(X[i][j]) < 0.0000001):
-            X[i][j] = 0
-        print("X[", i, "][", j, "]: ", X[i][j])
+    X[i]=abs(X[i])
+plt.pcolormesh(X,cmap='inferno')
+plt.colorbar()
+plt.xlabel("Frequency")
+plt.ylabel("Time")
+plt.title("Short Time Fourier Transform")
+plt.show()
