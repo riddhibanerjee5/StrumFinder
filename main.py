66982813391f62244c3cd67001a2b7a334d904a1
riddhibanerjee5 <riddhibanerjee@ufl.edu>
2022-11-16 01:11:08 -0500

    Add file explorer button

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from playsound import playsound
 from tkinter import *
 import multiprocessing
+from tkinter import filedialog
+
 
 root = Tk()
 root.title('Strum Finder')  # giving the title for our window
-root.geometry("500x400")
+root.geometry("1000x1000")
 
-# making function
-# p = multiprocessing.Process(target=playsound, args=("1.mp3",))
+
+def openFile():
+    filename = filedialog.askopenfilename(initialdir="/",
+                                          title="Select a File",
+                                          filetypes=(("wav files",
+                                                      "*.wav*"),
+
+                                                     ))
 
 
 def play():
@@ -20,7 +28,11 @@ title = Label(root, text="Strum Finder", bd=9, relief=GROOVE,
               font=("times new roman", 50, "bold"), bg="white", fg="blue")
 title.pack(side=TOP, fill=X)
 
-# making a button which trigger the function so sound can be playeed
+select_file_button = Button(root, text="Select File", font=(
+    "Helvetica", 16), relief=GROOVE, command=openFile)
+select_file_button.pack(pady=40)
+
+# making a button which trigger the function so sound can be played
 play_button = Button(root, text="Play Song", font=(
     "Helvetica", 16), relief=GROOVE, command=play)
 play_button.pack(pady=40)
